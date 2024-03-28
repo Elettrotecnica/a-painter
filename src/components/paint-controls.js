b57900c792a1109fd14176334e74a289a2f0d43f
Antonio Pisano <antonio@elettrotecnica.it>
2024-03-28 13:28:30 +0100

    Refactor the component to allow play/pause
    When paused, the component will stop listening to events and will set the mesh to invisible

--- src/components/paint-controls.js
+++ src/components/paint-controls.js
@@ -15,109 +15,24 @@ AFRAME.registerComponent('paint-controls', {
     var self = this;
     var highLightTextureUrl = 'assets/images/controller-pressed.png';
     var tooltipGroups = null;
+
     this.controller = null;
     this.modelLoaded = false;
-    
+    this.touchStarted = false;
+    this.startAxis = 0;
+    this.numberStrokes = 0;
+
+    this.el.object3D.visible = false;
+
     this.onEnterVR = this.onEnterVR.bind(this);
-    el.sceneEl.addEventListener('enter-vr', this.onEnterVR);
-    el.object3D.visible = false;
-
     this.onModelLoaded = this.onModelLoaded.bind(this);
-    el.addEventListener('model-loaded', this.onModelLoaded);
-
-    el.addEventListener('changeBrushSizeAbs', function (evt) {
-      if (evt.detail.axis[1] === 0 && evt.detail.axis[3] === 0) { return; }
-
-      var magnitude = evt.detail.axis[1] || evt.detail.axis[3];
-      var delta = magnitude / 300;
-      var size = el.components.brush.schema.size;
-      var value = THREE.MathUtils.clamp(self.el.getAttribute('brush').size - delta, size.min, size.max);
-
-      self.el.setAttribute('brush', 'size', value);
-    });
-
-    el.addEventListener('changeBrushSizeInc', function (evt) {
-      if (evt.detail.axis[1] === 0 && evt.detail.axis[3] === 0) { return; }
-
-      var magnitude = evt.detail.axis[1] || evt.detail.axis[3];
-
-      if (self.touchStarted) {
-        self.touchStarted = false;
-        self.startAxis = (magnitude + 1) / 2;
-      }
-
-      var currentAxis = (magnitude + 1) / 2;
-      var delta = (self.startAxis - currentAxis) / 2;
-
-      self.startAxis = currentAxis;
-
-      var startValue = self.el.getAttribute('brush').size;
-      var size = el.components.brush.schema.size;
-      var value = THREE.MathUtils.clamp(startValue - delta, size.min, size.max);
-
-      self.el.setAttribute('brush', 'size', value);
-    });
-
-    self.touchStarted = false;
-
-    el.addEventListener('startChangeBrushSize', function () {
-      self.touchStarted = true;
-    });
-
-    el.addEventListener('controllerconnected', function (evt) {
-      var controllerName = evt.detail.name;
-      var hand = evt.detail.component.data.hand;
-
-      const createBrushTip = (controllerVersion, hand) => {
-        // Create brush tip and position it dynamically based on our current controller
-        this.brushTip = document.createElement('a-entity');
-        this.brushTip.id = `${hand}-tip`;
-        this.brushTip.setAttribute('gltf-model', '#tipObj');
-        this.brushTip.setAttribute('brush-tip', `controller: ${controllerVersion}; hand: ${hand}`);
-        this.brushTip.addEventListener('model-loaded', self.onModelLoaded);
-        el.appendChild(this.brushTip);
-      }
-
-      if (controllerName === 'windows-motion-controls')
-      {
-        var gltfName = evt.detail.component.el.components['gltf-model'].data;
-        const SAMSUNG_DEVICE = '045E-065D';
-        if (!!gltfName)
-        {
-          if (gltfName.indexOf(SAMSUNG_DEVICE) >= 0)
-          {
-            controllerName = "windows-motion-samsung-controls";
-          }
-        }
-      }
-
-      tooltipGroups = Utils.getTooltips(controllerName);
-      if (controllerName.indexOf('windows-motion') >= 0) {
-        // el.setAttribute('teleport-controls', {button: 'trackpad'});
-      } else if (controllerName === 'oculus-touch-controls') {
-        const controllerModelURL = el.components[controllerName].displayModel[hand].modelUrl;
-        const versionMatchPattern = /[^\/]*(?=-(?:left|right)\.)/; // Matches the "oculus-touch-controller-[version]" part of URL
-        const controllerVersion = versionMatchPattern.exec(controllerModelURL)[0];
-        createBrushTip(controllerVersion, hand);
-      } else if (controllerName === 'vive-controls') {
-        el.setAttribute('gltf-model', 'url(assets/models/vive-controller.glb)');
-      } else { return; }
-
-      if (!!tooltipGroups) {
-        tooltipGroups.forEach(function (tooltipGroup) {
-          tooltipGroup.setAttribute('visible', true);
-          const tooltips = Array.prototype.slice.call(tooltipGroup.querySelectorAll('[tooltip]'));
-          tooltips.forEach(function (tooltip) {
-            tooltip.setAttribute('animation', { dur: 1000, delay: 2000, property: 'tooltip.opacity', from: 1.0, to: 0.0, startEvents: 'tooltip-fade' });
-          });
-        });
-      }
-
-      this.controller = controllerName;
-    });
-
-    el.addEventListener('brushsize-changed', function (evt) { self.changeBrushSize(evt.detail.size); });
-    el.addEventListener('brushcolor-changed', function (evt) { self.changeBrushColor(evt.detail.color); });
+    this.onChangeBrushSizeAbs = this.onChangeBrushSizeAbs.bind(this);
+    this.onChangeBrushSizeInc = this.onChangeBrushSizeInc.bind(this);
+    this.onStartChangeBrushSize = this.onStartChangeBrushSize.bind(this);
+    this.onControllerconnected = this.onControllerconnected.bind(this);
+    this.onBrushSizeChanged = this.onBrushSizeChanged.bind(this);
+    this.onBrushColorChanged = this.onBrushColorChanged.bind(this);
+    this.onStrokeStarted = this.onStrokeStarted.bind(this);
 
     function createTexture (texture) {
       var material = self.highLightMaterial = new THREE.MeshBasicMaterial();
@@ -125,25 +40,6 @@ AFRAME.registerComponent('paint-controls', {
       material.needsUpdate = true;
     }
     el.sceneEl.systems.material.loadTexture(highLightTextureUrl, {src: highLightTextureUrl}, createTexture);
-
-    this.startAxis = 0;
-
-    this.numberStrokes = 0;
-
-    document.addEventListener('stroke-started', function (event) {
-      if (event.detail.entity.components['paint-controls'] !== self) { return; }
-
-      self.numberStrokes++;
-      self.system.numberStrokes++;
-
-      // 3 Strokes to hide
-      if (self.system.numberStrokes === 3) {
-        const tooltips = Array.prototype.slice.call(document.querySelectorAll('[tooltip]'));
-        tooltips.forEach(function (tooltip) {
-          tooltip.emit('tooltip-fade');
-        });
-      }
-    });
   },
 
   changeBrushColor: function (color) {
@@ -184,10 +80,45 @@ AFRAME.registerComponent('paint-controls', {
     el.setAttribute('windows-motion-controls', {hand: data.hand});
   },
 
+  addEventListeners: function () {
+    this.el.sceneEl.addEventListener('enter-vr', this.onEnterVR);
+    this.el.addEventListener('model-loaded', this.onModelLoaded);
+    this.el.addEventListener('changeBrushSizeAbs', this.onChangeBrushSizeAbs);
+    this.el.addEventListener('changeBrushSizeInc', this.onChangeBrushSizeInc);
+    this.el.addEventListener('startChangeBrushSize', this.onStartChangeBrushSize);
+    this.el.addEventListener('controllerconnected', this.onControllerconnected);
+    this.el.addEventListener('brushsize-changed', this.onBrushSizeChanged);
+    this.el.addEventListener('brushcolor-changed', this.onBrushColorChanged);
+    document.addEventListener('stroke-started', this.onStrokeStarted);
+  },
+
+  removeEventListeners: function () {
+    this.el.sceneEl.removeEventListener('enter-vr', this.onEnterVR);
+    this.el.removeEventListener('model-loaded', this.onModelLoaded);
+    this.el.removeEventListener('changeBrushSizeAbs', this.onChangeBrushSizeAbs);
+    this.el.removeEventListener('changeBrushSizeInc', this.onChangeBrushSizeInc);
+    this.el.removeEventListener('startChangeBrushSize', this.onStartChangeBrushSize);
+    this.el.removeEventListener('controllerconnected', this.onControllerconnected);
+    this.el.removeEventListener('brushsize-changed', this.onBrushSizeChanged);
+    this.el.removeEventListener('brushcolor-changed', this.onBrushColorChanged);
+    document.removeEventListener('stroke-started', this.onStrokeStarted);
+  },
+
+  setModelVisibility: function (visible) {
+    const mesh = this.el.getObject3D('mesh');
+    if (mesh) {
+      mesh.visible = !!visible;
+    }
+  },
+
   play: function () {
+    this.addEventListeners();
+    this.setModelVisibility(true);
   },
 
   pause: function () {
+    this.removeEventListeners();
+    this.setModelVisibility(false);
   },
 
   onEnterVR: function () {
@@ -200,7 +131,7 @@ AFRAME.registerComponent('paint-controls', {
 
     var controllerObject3D = evt.detail.model;
     var buttonMeshes;
-    
+
     buttonMeshes = this.buttonMeshes = {};
 
     buttonMeshes.sizeHint = controllerObject3D.getObjectByName('sizehint');
@@ -210,6 +141,118 @@ AFRAME.registerComponent('paint-controls', {
 
     this.changeBrushSize(this.el.components.brush.data.size);
     this.changeBrushColor(this.el.components.brush.color);
+  },
+
+  onChangeBrushSizeAbs: function (evt) {
+    if (evt.detail.axis[1] === 0 && evt.detail.axis[3] === 0) { return; }
+
+    var magnitude = evt.detail.axis[1] || evt.detail.axis[3];
+    var delta = magnitude / 300;
+    var size = this.el.components.brush.schema.size;
+    var value = THREE.MathUtils.clamp(this.el.getAttribute('brush').size - delta, size.min, size.max);
+
+    this.el.setAttribute('brush', 'size', value);
+  },
+
+  onChangeBrushSizeInc: function (evt) {
+    if (evt.detail.axis[1] === 0 && evt.detail.axis[3] === 0) { return; }
+
+    var magnitude = evt.detail.axis[1] || evt.detail.axis[3];
+
+    if (this.touchStarted) {
+      this.touchStarted = false;
+      this.startAxis = (magnitude + 1) / 2;
+    }
+
+    var currentAxis = (magnitude + 1) / 2;
+    var delta = (this.startAxis - currentAxis) / 2;
+
+    this.startAxis = currentAxis;
+
+    var startValue = this.el.getAttribute('brush').size;
+    var size = this.el.components.brush.schema.size;
+    var value = THREE.MathUtils.clamp(startValue - delta, size.min, size.max);
+
+    this.el.setAttribute('brush', 'size', value);
+  },
+
+  onStartChangeBrushSize: function () {
+    this.touchStarted = true;
+  },
+
+  onControllerconnected: function (evt) {
+    var controllerName = evt.detail.name;
+    var hand = evt.detail.component.data.hand;
+
+    const createBrushTip = (controllerVersion, hand) => {
+      // Create brush tip and position it dynamically based on our current controller
+      this.brushTip = document.createElement('a-entity');
+      this.brushTip.id = `${hand}-tip`;
+      this.brushTip.setAttribute('gltf-model', '#tipObj');
+      this.brushTip.setAttribute('brush-tip', `controller: ${controllerVersion}; hand: ${hand}`);
+      this.brushTip.addEventListener('model-loaded', this.onModelLoaded);
+      this.el.appendChild(this.brushTip);
+    }
+
+    if (controllerName === 'windows-motion-controls')
+    {
+      var gltfName = evt.detail.component.el.components['gltf-model'].data;
+      const SAMSUNG_DEVICE = '045E-065D';
+      if (!!gltfName)
+      {
+        if (gltfName.indexOf(SAMSUNG_DEVICE) >= 0)
+        {
+          controllerName = "windows-motion-samsung-controls";
+        }
+      }
+    }
+
+    tooltipGroups = Utils.getTooltips(controllerName);
+    if (controllerName.indexOf('windows-motion') >= 0) {
+      // this.el.setAttribute('teleport-controls', {button: 'trackpad'});
+    } else if (controllerName === 'oculus-touch-controls') {
+      const controllerModelURL = this.el.components[controllerName].displayModel[hand].modelUrl;
+      const versionMatchPattern = /[^\/]*(?=-(?:left|right)\.)/; // Matches the "oculus-touch-controller-[version]" part of URL
+      const controllerVersion = versionMatchPattern.exec(controllerModelURL)[0];
+      createBrushTip(controllerVersion, hand);
+    } else if (controllerName === 'vive-controls') {
+      this.el.setAttribute('gltf-model', 'url(assets/models/vive-controller.glb)');
+    } else { return; }
+
+    if (!!tooltipGroups) {
+      tooltipGroups.forEach(function (tooltipGroup) {
+        tooltipGroup.setAttribute('visible', true);
+        const tooltips = Array.prototype.slice.call(tooltipGroup.querySelectorAll('[tooltip]'));
+        tooltips.forEach(function (tooltip) {
+          tooltip.setAttribute('animation', { dur: 1000, delay: 2000, property: 'tooltip.opacity', from: 1.0, to: 0.0, startEvents: 'tooltip-fade' });
+        });
+      });
+    }
+
+    this.controller = controllerName;
+  },
+
+  onBrushSizeChanged: function (evt) {
+    this.changeBrushSize(evt.detail.size);
+  },
+
+  onBrushColorChanged: function (evt) {
+    this.changeBrushColor(evt.detail.color);
+  },
+
+  onStrokeStarted: function (evt) {
+    if (evt.detail.entity.components['paint-controls'] !== this) { return; }
+
+    this.numberStrokes++;
+    this.system.numberStrokes++;
+
+    // 3 Strokes to hide
+    if (this.system.numberStrokes === 3) {
+      const tooltips = Array.prototype.slice.call(document.querySelectorAll('[tooltip]'));
+      tooltips.forEach(function (tooltip) {
+        tooltip.emit('tooltip-fade');
+      });
+    }
   },
 
   onButtonEvent: function (id, evtName) {
